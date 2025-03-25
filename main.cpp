dde6e1f72e702367b72e24f4c121fdc37d8c999a
DanielSSamaniego <a24110211@live.ceti.mx>
2025-03-25 23:02:23 +0000

    version 0

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Foco.hpp"
+
+int main(int argc, char const *argv[])
+{
+
+    Foco focos[10];
+    for (size_t i = 0; i < 10; i++)
+    {
+       focos[i].Encender();
+    }
+    
+   // foco1.Encender();
+
+    std::cout
+        << "Foco 1: "
+        << foco1.LeerEstado()
+        << std::endl;
+
+    std::cout
+        << "argc:"
+        << argc
+        << std::endl;
+
+    return 0;
+}
