066dbda7818df14511e4169a8b804f0a3dbfa656
DanielSSamaniego <a24110211@live.ceti.mx>
2025-04-30 00:15:52 +0000

    Practica 5

--- src/archivos.cpp
+++ src/archivos.cpp
@@ -3,6 +3,11 @@
 #include <string>
 using namespace std;
 
+struct Persona{
+    char nombre[10];
+    int edad;
+};
+
 int main(int argc, char const *arg[])
 {
     ifstream archivoSalida("archivo.txt");
@@ -20,6 +25,23 @@ int main(int argc, char const *arg[])
     }
 
     archivoSalida.close();
+//////****************************************/////
+//Escritura de archivos binarios
+
+ofstream archivoSalidaBinario("archivo.bin",ios::binary);
+if(!archivoSalidaBinario){
+    cerr << "Error al leer archivo" << endl;
+    return 1; // Finalizar con error
+}
+
+Persona p1 = {"Juan", 20};
+Persona p2 = {"Pedro", 30};
+Persona p3 = {"Maria", 40};
+
+archivoSalidaBinario.write((char*)(&p1), sizeof(Persona));
+archivoSalidaBinario.write((char*)(&p2), sizeof(Persona));
+archivoSalidaBinario.write((char*)(&p3), sizeof(Persona));
+archivoSalidaBinario.close();
 
     return 0;
 }
